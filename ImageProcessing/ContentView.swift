f2cfdb63394687261e54f0aa952f2e97922ede81
hajarAlshehri22 <147072959+hajarAlshehri22@users.noreply.github.com>
2023-12-14 17:20:08 +0300

    Initial Commit

---
+++ ImageProcessing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ImageProcessing
+//
+//  Created by Hajar Alshehri on 01/06/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ImageProcessing/ImageProcessingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageProcessingApp.swift
+//  ImageProcessing
+//
+//  Created by Hajar Alshehri on 01/06/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct ImageProcessingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
