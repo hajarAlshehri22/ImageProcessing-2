f233447a9ed6d2698ac180c9ee7b5e5ea1be3218
ealmalki23 <147074692+ealmalki23@users.noreply.github.com>
2023-12-24 16:55:12 +0300

    SplashScreen

---
+++ ImageProcessing/SplashScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreen.swift
+//  ImageProcessing
+//
+//  Created by Eman Almalki  on 11/06/1445 AH.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreen()
+}
