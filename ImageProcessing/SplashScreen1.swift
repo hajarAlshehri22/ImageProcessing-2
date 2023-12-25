758b8afba9e2ef12c0fb47e0edf30d494ea63da6
hajarAlshehri22 <147072959+hajarAlshehri22@users.noreply.github.com>
2023-12-25 18:00:05 +0300

    last

---
+++ ImageProcessing/SplashScreen1.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreen1.swift
+//  ImageProcessing
+//
+//  Created by Hajar Alshehri on 12/06/1445 AH.
+//
+
+import SwiftUI
+
+struct SplashScreen1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreen1()
+}
